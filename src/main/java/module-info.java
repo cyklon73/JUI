fd94126515d1fee7dd71eb33f798bfa398a1cd8d
cyklon73 <cyklon698@gmail.com>
2024-01-02 18:56:11 +0100

    added module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,16 @@ module JUI {
 	requires JEvent;
 	requires JResource;
 	requires java.desktop;
+
+	exports de.cyklon.jui;
+	exports de.cyklon.jui.component;
+	exports de.cyklon.jui.cursor;
+	exports de.cyklon.jui.event;
+	exports de.cyklon.jui.event.window;
+	exports de.cyklon.jui.input;
+	exports de.cyklon.jui.render;
+	exports de.cyklon.jui.splash;
+	exports de.cyklon.jui.task;
+	exports de.cyklon.jui.tuple;
+
 }
