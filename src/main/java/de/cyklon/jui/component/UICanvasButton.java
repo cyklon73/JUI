641b7bdcb1c8b0bee1f3d8dd39609819be2953a8
cyklon73 <cyklon698@gmail.com>
2024-04-10 07:33:07 +0200

    added Supplier constructor

--- src/main/java/de/cyklon/jui/component/UICanvasButton.java
+++ src/main/java/de/cyklon/jui/component/UICanvasButton.java
@@ -2,9 +2,15 @@ package de.cyklon.jui.component;
 
 import de.cyklon.jui.UICanvas;
 
+import java.util.function.Supplier;
+
 public class UICanvasButton extends UIButton {
+
 	public UICanvasButton(int x, int y, int width, int height, UICanvas canvas) {
-		super(x, y, width, height, (app, btn) -> app.setCanvas(canvas));
+		this(x, y, width, height, () -> canvas);
+	}
+	public UICanvasButton(int x, int y, int width, int height, Supplier<UICanvas> canvas) {
+		super(x, y, width, height, (app, btn) -> app.setCanvas(canvas.get()));
 	}
 
 }
