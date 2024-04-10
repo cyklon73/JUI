e9fb18058dc8854fc080f3059c314054eb4bee4d
cyklon73 <cyklon698@gmail.com>
2024-04-10 07:52:58 +0200

    added Function constructor

--- src/main/java/de/cyklon/jui/component/UICanvasButton.java
+++ src/main/java/de/cyklon/jui/component/UICanvasButton.java
@@ -1,16 +1,22 @@
 package de.cyklon.jui.component;
 
+import de.cyklon.jui.App;
 import de.cyklon.jui.UICanvas;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class UICanvasButton extends UIButton {
 
 	public UICanvasButton(int x, int y, int width, int height, UICanvas canvas) {
-		this(x, y, width, height, () -> canvas);
+		this(x, y, width, height, app -> canvas);
 	}
-	public UICanvasButton(int x, int y, int width, int height, Supplier<UICanvas> canvas) {
-		super(x, y, width, height, (app, btn) -> app.setCanvas(canvas.get()));
+
+    public UICanvasButton(int x, int y, int width, int height, Supplier<UICanvas> canvas) {
+        this(x, y, width, height, app -> canvas.get());
+    }
+	public UICanvasButton(int x, int y, int width, int height, Function<App, UICanvas> canvas) {
+		super(x, y, width, height, (app, btn) -> app.setCanvas(canvas.apply(app)));
 	}
 
 }
